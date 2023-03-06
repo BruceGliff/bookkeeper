e5403d6c04d82fabb7222f5204aa27fb1e7ef957
BruceGliff <ilyaawaA@yandex.ru>
2023-03-06 09:42:19 -0500

    [L8.0] Change category view to tree.

--- bookkeeper/view/edit_ctg_window.py
+++ bookkeeper/view/edit_ctg_window.py
@@ -3,12 +3,14 @@ Widget of editing categories
 """
 
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import (QWidget, QListWidget)
+from PySide6.QtWidgets import (QWidget, QTreeWidget, QTreeWidgetItem)
 
 
-def set_data(table: QListWidget, data: list[str]) -> None:
-    for ctg in data:
-        table.addItem(ctg)
+def set_data(table: QTreeWidget, data: list[str]) -> None:
+    stf = QTreeWidgetItem(table, ['1 Продукты'])
+    book = QTreeWidgetItem(table, ['1 Книги'])
+    meet = QTreeWidgetItem(stf, ['2 Мясо'])
+    fish = QTreeWidgetItem(stf, ['2 Рыба'])
 
 
 class EditCtgWindow(QWidget):
@@ -18,10 +20,10 @@ class EditCtgWindow(QWidget):
         self.setWindowTitle("Изменение категорий")
 
         layout = QtWidgets.QVBoxLayout()
-        message = QtWidgets.QLabel("Категории")
-        layout.addWidget(message)
-
-        ctgs_widget = QtWidgets.QListWidget()
+        
+        ctgs_widget = QtWidgets.QTreeWidget()
+        ctgs_widget.setColumnCount(1)
+        ctgs_widget.setHeaderLabel('Категории')
         set_data(ctgs_widget, ctgs)
 
         layout.addWidget(ctgs_widget)
